1cb6f035a621cc172ffa33a687b331aac623b3bd
Mia <mia@vidops.io>
2020-11-23 17:43:14 +0000

    commented out the preview + key method for retrieving data. Created new datatype in Parse for grabbing Object

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,22 @@ module Main where
 import HTTP
 import Parse
 
+
 main :: IO ()
 main = do
     let url = "https://api.coindesk.com/v1/bpi/currentprice.json"
     json <- download url
-    print (getRateGBP json)
-    print (getRateUS json)
+    case (parse json) of
+        Left err -> print err
+        Right bits -> print (parse json)
+    
 
- -- next we would add / capture more data from the json
- -- then we could add cases for our queries e.g. case get GBP > then say this etc
+
+
+ -- commented out for now - this works with the key function in Parse   
+ --   json <- download url
+ --   print (getRateGBP json)
+ --   print (getRateUS json)
+
+
+-- print gbp (putStrLn . lines . map show )
--- src/Parse.hs
+++ src/Parse.hs
@@ -14,10 +14,43 @@ import Data.Text ( Text )
 -- added ,lens to cabal file build deps
 import Control.Lens ( preview )
 
+-- create a datatype for the USD data structure 
+data USD = USD {
+    code :: String,
+    rate :: String
+} deriving (Show, Generic)
+
+instance FromJSON USD
+instance ToJSON USD
+
+-- create a datatype for the BPIs data structure with all the currency's
+
+data BPIS = BPIS {
+   usd :: Object,
+   gbp :: Object,
+   eur :: Object
+} deriving (Show, Generic)
+
+instance FromJSON BPIS
+instance ToJSON BPIS
+
+-- create a datatype for the BPI key 
 
 
-getRateGBP :: L8.ByteString -> Maybe Text
-getRateGBP = preview (key "bpi" . key "GBP" . key "rate" . _String)
+data BPI = BPI {
+   bpi :: Object
+} deriving (Show, Generic)
 
-getRateUSD :: L8.ByteString -> Maybe Text
-getRateUSD = preview (key "bpi" . key "USD" . key "rate" . _String)
+instance FromJSON BPI
+instance ToJSON BPI
+
+parse :: L8.ByteString -> Either String BPI
+parse json = eitherDecode json :: Either String BPI
+
+
+-- commented out for now, this works with the print function commented out on main
+--getRateGBP :: L8.ByteString -> Maybe Text
+--getRateGBP = preview (key "bpi" . key "GBP" . key "rate" . _String)
+
+--getRateUSD :: L8.ByteString -> Maybe Text
+--getRateUSD = preview (key "bpi" . key "USD" . key "rate" . _String)
